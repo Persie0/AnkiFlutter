caee56c1be1980c7dc3d79a1538b35317a1940a8
Persie0 <76871553+Persie0@users.noreply.github.com>
2026-09-26 16:02:34 +0200

    feat: wire production desktop root

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:anki_flutter/app/anki_desktop_root.dart';
 import 'package:anki_flutter/app/anki_flutter_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const AnkiFlutterApp(home: Placeholder()));
+  runApp(const AnkiFlutterApp(home: AnkiDesktopRoot()));
 }
